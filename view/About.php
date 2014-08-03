e218230e1366e63be6cfbe4b282c518d4839815e
Julien Boudry <julien.boudry@supinternet.fr>
2014-08-03 11:01:41 +0200

    Page about & Autres

--- view/About.php
+++ view/About.php
@@ -1,5 +1,28 @@
 <div class="container">
 
-	About
+	<section class="home-section">
+		<header class="text-center">
+			<img src="<?php echo BASE_URL . 'view/IMG/condorcet-black.png'; ?>" alt="condorcet black">
+			<h2>Condorcet-Vote.org ?</h2>
+		</header>
+		<p>Condorcet-Vote.org provides a simple and comprehensive way to promote the use of alternative voting systems from the Marquis de Condorcet method.
+		We bring no warranty as to the results obtained, and we reserve the right to discontinue service at any time of general or specific.</p>
+
+		<p>Condorcet-Vote is based on a number of free technology, some derived created for the occasion. Consider, however, any other element is the exclusive property of Mr. <a href="http://www.julien-boudry.net" target="_blank">Julien Boudry</a> and is governed by French law.</p>
+	</section>
+	<hr class="clearfix">
+
+	<section class="home-section">
+		<header class="text-center">
+			<a href="http://github.com/julien-boudry/Condorcet" target="_blank"><img src="https://raw.githubusercontent.com/julien-boudry/Condorcet/master/condorcet-logo.png" alt="Condorcet PHP logo" class="home-icon"></a>
+			<h2>Condorcet PHP</h2>
+		</header>
+		<p>Condorcet-Vote is largely based on the open source Condorcet PHP library, using it to calculate both the election results for its advanced functions of micro-framework of election management.</p>
+
+		<p>And because Condorcet Voting has been designed and developed by the same person. You can find all the sources, manuals and help on <a href="http://github.com/julien-boudry/Condorcet" target="_blank">Github</a>.</p>
+
+		<div class="text-center"><a href="http://github.com/julien-boudry/Condorcet" target="_blank"><img src="<?php echo BASE_URL; ?>view/IMG/GitHub_Logo.png" alt="GitHub logo" style="max-width:150px;"></a></div>
+	</section>
+	<hr class="clearfix">
 
 </div> <!-- /container -->
--- view/Skeleton/footer.php
+++ view/Skeleton/footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">&#xa9; Condorcet-Vote</p>
+		<div class="text-muted pull-left">&#xa9; Condorcet-Vote</div>
+		<div class="text-muted pull-right"><a href="<?php echo BASE_URL ; ?>About">About</a></div>
 	</div>
 </footer>
 
