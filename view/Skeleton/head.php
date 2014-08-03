9703182abfd49edb6160472fab7354c36ddb8a24
Julien Boudry <julien.boudry@supinternet.fr>
2014-08-03 10:29:23 +0200

    Meta Keywords

--- view/Skeleton/head.php
+++ view/Skeleton/head.php
@@ -6,6 +6,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<meta name="keywords" content="Condorcet, Vote System, Schulze, Condorcet Method, Online poll">
 
 	<?php Controller::setHead(); ?>
 
