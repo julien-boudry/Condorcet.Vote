cc01e800518a575de24cfa3a317b8a7d8fc641b0
Julien Boudry <julien.boudry@supinternet.fr>
2014-09-14 16:21:38 +0200

    Indentation de functions.php

--- model/functions.php
+++ model/functions.php
@@ -40,35 +40,35 @@
 		}
 	}
 
-set_error_handler(function($type, $msg, $file, $line, $context = array()) {
+	set_error_handler(function($type, $msg, $file, $line, $context = array()) {
 
-   if (CONFIG_ENV === 'DEV') {return FALSE ;}
+		if (CONFIG_ENV === 'DEV') {return FALSE ;}
 
-    Events::add(new Error(500, null, $type.' - '.$msg, null, 2, 3) );
+		Events::add(new Error(500, null, $type.' - '.$msg, null, 2, 3) );
 
-    ( new Error_Controller() )->showPage();
+		( new Error_Controller() )->showPage();
 
-    exit();
-});
+		exit();
+	});
 
-// Contenu des aides
+	// Contenu des aides
 
-function setHelper($type, $color = 'info')
-{
-	echo '<aside class="bs-callout bs-callout-'.$color.'">';
+	function setHelper($type, $color = 'info')
+	{
+		echo '<aside class="bs-callout bs-callout-'.$color.'">';
 
-	switch ($type) {
-		case 'enter_candidates':
-			echo
-				'Each separated by a semicolon candidate. A candidate may take up to 30 alphanumeric characters but can not be a number. The front and rear spaces will be ignored.
-				Of course, a minimum vote requires two candidates.';
-			break;
+		switch ($type) {
+			case 'enter_candidates':
+				echo
+					'Each separated by a semicolon candidate. A candidate may take up to 30 alphanumeric characters but can not be a number. The front and rear spaces will be ignored.
+					Of course, a minimum vote requires two candidates.';
+				break;
 
-		case 'enter_votes':
-			echo
-				"
-				<h4>Syntax</h4>
-				Example with 4 candidates : A;B;C;D
+			case 'enter_votes':
+				echo
+					"
+<h4>Syntax</h4>
+Example with 4 candidates : A;B;C;D
 <pre><code>tag1,tag2,tag3[...] || A>B=D>C # A comment at the end of the line prefixed by '#'. Never use ';' in comment!
 Titan,CoteBoeuf || A>B=D>C # Tags at first, vote at second, separated by '||'
 A>C>D>B # Line break to start a new vote. Tags are optionals. View above for vote syntax.
@@ -78,33 +78,35 @@ C>D>B*8;A=D>B;Julien,Christelle||A>C>D>B*4;D=B=C>A # Alternatively, you can repl
 B>C # Equivalent to B>C>A=D</code></pre>";
 			break;
 
-	case 'methods_infos':
-		echo
-			'All these methods meet the criteria of Condorcet. For more information on the different methods of calculation, look <a href="'.BASE_URL.'Condorcet_Methods" target="_blank">this page</a>.';
-		break;
+		case 'methods_infos':
+			echo
+				'All these methods meet the criteria of Condorcet. For more information on the different methods of calculation, look <a href="'.BASE_URL.'Condorcet_Methods" target="_blank">this page</a>.';
+			break;
 
-	case 'delete_infos':
-		echo
-			'Vote tag or number separated by semicolon. The operation will be performed before adding your new votes.';
-		break;
+		case 'delete_infos':
+			echo
+				'Vote tag or number separated by semicolon. The operation will be performed before adding your new votes.';
+			break;
 
-	case 'personnal_vote':
-		echo
-			'The staff is used to allow voting once the voting for a person identified by name. The vote will then be provided specifically identifiable by a tag matching that name. <br>
-			You must individually contact the url provided to the voter. ';
-		break;
+		case 'personnal_vote':
+			echo
+				'The staff is used to allow voting once the voting for a person identified by name. The vote will then be provided specifically identifiable by a tag matching that name. <br>
+				You must individually contact the url provided to the voter. ';
+			break;
 
-	case 'public_voting':
-		echo 'If checked the vote will be disabled as well as the URL of public vote that personal URL.';
-		break;
+		case 'public_voting':
+			echo 'If checked the vote will be disabled as well as the URL of public vote that personal URL.';
+			break;
 
-	case 'reset_url':
-		echo 'Warning: Disable and re-enable the external voting also resets all URL. Public or personnal vote.';
-		break;
+		case 'reset_url':
+			echo 'Warning: Disable and re-enable the external voting also resets all URL. Public or personnal vote.';
+			break;
 
-		default:
-			echo '????';
+			default:
+				echo '????';
+		}
+
+		echo '</aside>';	
 	}
 
-	echo '</aside>';	
-}
+
