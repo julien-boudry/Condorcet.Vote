a4752fb91fdd7cb63b4a5bc0782eaf09271290b5
Julien Boudry <julien.boudry@supinternet.fr>
2015-01-30 00:33:31 +0100

    Disable auto-update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ define('SCRIPT_START',microtime(true));
 	require_once 'controllers/main.php';
 
 // Update Script
-	require_once 'update.php';
+	# require_once 'update.php';
 
 	if (isset($_GET['ajax']))
 		{Controller::$_ajax = true;}
