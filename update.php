a0225e884be004d6c635df59fe8321f10587e911
Julien Boudry <julien.boudry@gmail.com>
2017-12-30 21:28:18 +0100

    Fix update memory limit

--- update.php
+++ update.php
@@ -7,14 +7,14 @@ set_time_limit ( 600 );
 $manager = new Condorcet\Timer\Manager;
 $chrono = new Condorcet\Timer\Chrono ($manager);
 
-$to_update = R::find( 'condorcet', " condorcet_version != -".Condorcet\Condorcet::getVersion('MAJOR') );
+# $to_update = R::find( 'condorcet', " condorcet_version != -".Condorcet\Condorcet::getVersion('MAJOR') );
 
 unset($chrono);
 $looking = $manager->getLastTimer();
 
 $chrono = new Condorcet\Timer\Chrono ($manager);
 
-foreach ($to_update as $election)
+while (!empty($election = R::findOne('condorcet',  " condorcet_version != -".Condorcet\Condorcet::getVersion('MAJOR'))))
 {
     $nm = unserialize($election->methods);
 
@@ -31,11 +31,13 @@ foreach ($to_update as $election)
     unset($checkMethod);
 
     $election->methods = serialize($nm);
+
+    R::store($election);
 }
 
-foreach ($to_update as $election)
+while (!empty($election = R::findOne('condorcet',  " condorcet_version != -".Condorcet\Condorcet::getVersion('MAJOR'))))
 {
-	( new Condorcet_Vote ($election->read_code) );
+    ( new Condorcet_Vote ($election->read_code) );
 }
 
 unset($chrono);
