be07786b7201878de995901db3280a1c00a48f1a
Julien Boudry <julien.boudry@gmail.com>
2021-12-13 16:59:59 +0100

    Fix update

--- update.php
+++ update.php
@@ -9,14 +9,12 @@ set_time_limit ( 600 );
 $manager = new CondorcetPHP\Condorcet\Timer\Manager;
 $chrono = new CondorcetPHP\Condorcet\Timer\Chrono ($manager);
 
-# $to_update = R::find( 'condorcet', " condorcet_version != -".CondorcetPHP\Condorcet\Condorcet::getVersion('MAJOR') );
-
 unset($chrono);
 $looking = $manager->getLastTimer();
 
 $chrono = new CondorcetPHP\Condorcet\Timer\Chrono ($manager);
 
-while (!empty($election = R::findOne('condorcet',  " condorcet_version != -".CondorcetPHP\Condorcet\Condorcet::getVersion('MAJOR'))))
+while (!empty($election = R::findOne('condorcet',  " condorcet_version != -".CondorcetPHP\Condorcet\Condorcet::getVersion(true))))
 {
     ( new Condorcet_Vote ($election->read_code) );
 }
